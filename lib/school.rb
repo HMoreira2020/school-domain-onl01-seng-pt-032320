235de05f4baedf00263e4330ec16b8c7d204bd4b
Heather Moreira <moreiraheather@gmail.com>
2020-04-03 01:42:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,7 @@ class School
   end 
   
   def add_student(student_name, grade_level) 
-    if roster[grade_level] = grade_level 
+    if roster[grade_level] != nil 
       roster [grade_level] << student_name
     else 
       roster[grade_level] = []
