36990abb630d6ebdbc4d0c83f9c50fc2a44d557a
Heather Moreira <moreiraheather@gmail.com>
2020-04-03 01:45:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,10 @@ class School
   
   def initialize(school_name) 
     @name = school_name
-    @roster = {}
+  end 
+  
+  def roster
+    @roster = {} 
   end 
   
   def add_student(student_name, grade_level) 
