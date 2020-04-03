80ae461e71be15410ccb3fe0b7b79f5e6ab24b80
Heather Moreira <moreiraheather@gmail.com>
2020-04-03 02:11:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+require 'pry'
+
+class School 
+  
+  def initialize(school_name) 
+    @name = school_name
+    @roster = {} 
+  end 
+  
+  def roster
+    @roster 
+  end 
+  
+  def add_student(student_name, grade_level) 
+    if @roster[grade_level] 
+      @roster [grade_level] << student_name
+    else 
+      @roster[grade_level] = [] 
+      @roster[grade_level] << student_name  
+    end 
+  end 
+ 
+  
+  def grade(grade_level) 
+    @roster[grade_level] 
+  end 
+
+  def sort
+    @roster.collect{|grade_level, student_list| @roster[grade_level] = student_list.sort}
+    @roster
+  end 
+  
+end 
+  
+  
+  
+    
